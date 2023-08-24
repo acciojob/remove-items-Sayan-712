111e877ebf68f8e6d43077bda8e227a9016d2a40
Sayan-712 <134630207+Sayan-712@users.noreply.github.com>
2023-08-25 03:34:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 //your JS code here. If required.
-function removeCol() 
-{
-let x=document.getElementById("colorSelect");
-	x.remove(x.selectedIndex)
-}
+const dropDown=document.querySelector("#colorSelect");
+const btn=document.getElementByTagName("input")[0];
+btn.addEventListener("click",() =>
+	{
+		let option=document.getElementByTagName("option");
+		for(let i=0;i<option.length;i++)
+			if(option[i].innerText===dropDown.value)
+				return dropDown.removeChild(option[i])
+	});
